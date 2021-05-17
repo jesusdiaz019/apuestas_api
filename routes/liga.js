30b7728e0daacc60f4c5127956924253e82a6f94
Jesus Diaz <jesus_ad019@hotmail.com>
2021-05-17 12:16:05 -0500

    version 17/05/2021 12:16pm

--- routes/liga.js
+++ routes/liga.js
@@ -12,7 +12,7 @@ const {API_FOOTBALL_LEAGUE, API_KEY, API_HOST} = process.env;
 router.get('/save', cors(), async (req, res) => {
 
     try {
-        var api_req = unirest("GET", API_FOOTBALL_LEAGUE);
+        var api_req = await unirest("GET", API_FOOTBALL_LEAGUE);
         var pais = req.query.pais;
         var temporada = req.query.temporada;
         var estado = req.query.estado;
@@ -31,11 +31,16 @@ router.get('/save', cors(), async (req, res) => {
             if (api_res.error){
                 res.json(api_res.error);
             }else{
-                var data_length = api_res.body.response.length;
-                for (i=0;i<data_length;i++){
-                    registrarLista(api_res.body.response[i]);
+                try {
+                    let pool = await sql.connect(config);
+                    let result = await pool.request()
+                        .input('PaisLiga', sql.VarChar(20), api_res.body.response[0].country.name)
+                        .input('Ligas', sql.VarChar(MAX), api_res.body.response)
+                        .execute('GuardarPaisLiga');
+                    res.json(result.recordsets);
+                } catch (error) {
+                    res.json(error);
                 }
-                res.json(api_res.body.response);
             }
         });
         
@@ -58,24 +63,5 @@ router.get('/list', cors(), async (req, res) => {
     }
 
 });
-
-async function registrarLista(data){
-    try {
-        var liga_id = parseInt(data.league.id);
-        let pool = await sql.connect(config);
-        let result = await pool.request()
-            .input('CodigoLiga', sql.Int, liga_id)
-            .input('Liga', sql.VarChar(35), data.league.name)
-            .input('TipoLiga', sql.VarChar(15), data.league.type)
-            .input('Logo', sql.VarChar(60), data.league.logo)
-            .input('Pais', sql.VarChar(15), data.country.name)
-            .input('FechaInicio', sql.Date, data.seasons[0].start)
-            .input('FechaFin', sql.Date, data.seasons[0].end)
-            .input('Estado', sql.Bit, data.seasons[0].current)
-            .execute('GuardarLiga');
-    } catch (error) {
-        console.log(error);
-    }
-}
 
 module.exports = router;
